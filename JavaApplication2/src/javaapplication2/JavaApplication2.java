7785954a89d7a525d60bc2fb67e13030454afb3b
sravyasri55 <89533757+sravyasri55@users.noreply.github.com>
2021-09-01 21:27:35 -0500

    Committing Hello world

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic 
-        System.out.println("Hello World");
+        System.out.println("Hello World ");
     }
     
     
